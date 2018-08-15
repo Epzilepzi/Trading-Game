94c08f2e37dfd0974b04afd2f8cd1b2fd7ab35b4
Epzilepzi <epzilepzi@gmail.com>
2018-08-15 22:11:48 +1000

    Test

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-const {app, BrowserWindow} = require('electron')
-  
-  function createWindow () {
-    // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
-  
-    // and load the index.html of the app.
-    win.loadFile('index.html')
-  }
-  
-  app.on('ready', createWindow)
+const {app, BrowserWindow, ipcMain} = require('electron');
+const {autoUpdater} = require("electron-updater");
+let win; // this will store the window object
+
+// creates the default window
+function createDefaultWindow() {
+    win = new BrowserWindow({width: 900, height: 680});
+    win.loadFile('index.html');
+    win.on('closed', () => app.quit());
+  return win;
+}
+
+// when the app is loaded create a BrowserWindow and check for updates
+app.on('ready', function() {
+  createDefaultWindow()
+  autoUpdater.checkForUpdates();
+});
+
+// when the update has been downloaded and is ready to be installed, notify the BrowserWindow
+autoUpdater.on('update-downloaded', (info) => {
+    win.webContents.send('updateReady')
+});
+
+// when receiving a quitAndInstall signal, quit and install the new version ;)
+ipcMain.on("quitAndInstall", (event, arg) => {
+    autoUpdater.quitAndInstall();
+})
