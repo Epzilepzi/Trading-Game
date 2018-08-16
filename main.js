c640a1d70fb4e9c62db6c676c986d3560fac93be
Epzilepzi <epzilepzi@gmail.com>
2018-08-16 11:34:17 +1000

    Update v0.0.2

--- main.js
+++ main.js
@@ -1,27 +1,68 @@
-const {app, BrowserWindow, ipcMain} = require('electron');
+const {app, BrowserWindow, Menu, protocol, ipcMain} = require('electron');
 const {autoUpdater} = require("electron-updater");
-let win; // this will store the window object
+const {productURL} = 'https://github.com/Epzilepzi/Trading-Game/releases'
 
-// creates the default window
+/* menu */
+let template = []
+if (process.platform === 'darwin') {
+  // OS X
+  const name = app.getName();
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: 'About ' + name,
+        role: 'about'
+      },
+      {
+        label: 'Quit',
+        accelerator: 'Command+Q',
+        click() { app.quit(); }
+      },
+    ]
+  })
+}
+
+let win;
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
+
 function createDefaultWindow() {
-    win = new BrowserWindow({width: 900, height: 680});
-    win.loadFile('index.html');
-    win.on('closed', () => app.quit());
+  win = new BrowserWindow({width: 900, height: 680});
+  win.loadFile(`index.html`);
   return win;
 }
 
-// when the app is loaded create a BrowserWindow and check for updates
-app.on('ready', function() {
-  createDefaultWindow()
-  autoUpdater.checkForUpdates();
-});
-
-// when the update has been downloaded and is ready to be installed, notify the BrowserWindow
-autoUpdater.on('update-downloaded', (info) => {
-    win.webContents.send('updateReady')
-});
-
-// when receiving a quitAndInstall signal, quit and install the new version ;)
-ipcMain.on("quitAndInstall", (event, arg) => {
-    autoUpdater.quitAndInstall();
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
 })
+autoUpdater.on('update-available', (ev, info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (ev, info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (ev, err) => {
+  sendStatusToWindow('Error in auto-updater.');
+})
+autoUpdater.on('download-progress', (ev, progressObj) => {
+  sendStatusToWindow('Download progress...');
+})
+autoUpdater.on('update-downloaded', (ev, info) => {
+  sendStatusToWindow('Update downloaded; will install in 5 seconds');
+});
+
+app.on('ready', function() {
+  // Create the Menu
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  createDefaultWindow();
+});
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
